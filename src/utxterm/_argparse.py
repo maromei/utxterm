cc087f1aec0c7824da5b0331931c12adc79ddef9
maromei <maromei@proton.me>
2026-02-17 20:32:24 +0100

    Added an argument for 'mode'.

--- src/utxterm/_argparse.py
+++ src/utxterm/_argparse.py
@@ -2,11 +2,14 @@ import argparse
 from typing import Any
 from dataclasses import dataclass
 
+from utxterm._replace_formatting import ReplaceMode
+
 
 @dataclass
 class CliArgs:
     filepath: str
     verbose: bool
+    mode: ReplaceMode
 
 
 def setup_argparse() -> CliArgs:
@@ -34,8 +37,27 @@ def setup_argparse() -> CliArgs:
         help="Increase output verbosity",
     )
 
+    parser.add_argument(
+        "-m",
+        "--mode",
+        default=ReplaceMode.align_left.value,
+        choices=ReplaceMode.as_list(),
+    )
+
     args: argparse.Namespace = parser.parse_args()
     args_dict: dict[str, Any] = vars(args)
+
+    # The following error checks should never happen due to the parser
+    # alredy checking for the errors.
+    # Still done here to not assume errors.
+    mode_str: str | None = args_dict.get("mode")
+    if mode_str is None:
+        raise ValueError("No 'mode' setting passed by the argument parser.")
+    mode: ReplaceMode | None = ReplaceMode.get_by_value(mode_str)
+    if mode is None: 
+        raise ValueError("Invalid mode passed")
+    args_dict["mode"] = mode
+
     cli_args: CliArgs = CliArgs(**args_dict)
 
     return cli_args
