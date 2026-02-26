0213a9e1e688880366591405d4824c6750b6bffe
maromei <47637017+maromei@users.noreply.github.com>
2026-02-26 12:30:34 +0100

    Added multiple modes for replacing the style definitions (#1)
    Added the `--modes` flag.
    
    It contains 4 modes:
    
    # simple
    
    Simply replace every formatting sequence with a space.
    
    ```
    | before <color:blue>Blue Text</color> after |
    | before             Blue Text         after |
    ```
    
    # align_left
    
    The replacement whitespace with will be added to the right of
    **all following** text.
    
    ```
    | before <color:blue>Blue Text</color> after |
    | before Blue Text after                     |
    ```
    
    # center_ws
    
    Centers the text **within** the replacement whitespace.
    In case of uneven whitespace, the right side will contain one
    more space than the left side.
    
    ```
    | before <color:blue>Blue Text</color> after |
    | before           Blue Text           after |
    ```
    
    # center_line
    
    Centers the line within the bounds.
    
    ```
    | before <color:blue>Blue Text</color> after |
    |           before Blue Text after           |
    ```
    
    ---------
    
    Co-authored-by: maromei <maromei@proton.me>

--- src/utxterm/_argparse.py
+++ src/utxterm/_argparse.py
@@ -2,11 +2,14 @@ import argparse
 from typing import Any
 from dataclasses import dataclass
 
+from utxterm._replace_formatting import ReplaceMode
+
 
 @dataclass
 class CliArgs:
     filepath: str
     verbose: bool
+    mode: ReplaceMode
 
 
 def setup_argparse() -> CliArgs:
@@ -16,6 +19,7 @@ def setup_argparse() -> CliArgs:
             "View ASCII-based or unicode-based puml diagrams "
             "in your terminal with color."
         ),
+        formatter_class=argparse.RawTextHelpFormatter,
     )
 
     parser.add_argument(
@@ -34,8 +38,61 @@ def setup_argparse() -> CliArgs:
         help="Increase output verbosity",
     )
 
+    parser.add_argument(
+        "-m",
+        "--mode",
+        default=ReplaceMode.align_left.value,
+        choices=ReplaceMode.as_list(),
+        help=(
+            "Defines how the Tags definining the style are replaced.\n"
+            "\n"
+            "# simple\n"
+            "\n"
+            "Simply replace every formatting sequence with a space.\n"
+            "\n"
+            "| before <color:blue>Blue Text</color> after |\n"
+            "| before             Blue Text         after |\n"
+            "\n"
+            "# align_left\n"
+            "\n"
+            "The replacement whitespace with will be added to the right of\n"
+            "**all following** text.\n"
+            "\n"
+            "| before <color:blue>Blue Text</color> after |\n"
+            "| before Blue Text after                     |\n"
+            "\n"
+            "# center_ws\n"
+            "\n"
+            "Centers the text **within** the replacement whitespace.\n"
+            "In case of uneven whitespace, the right side will contain one\n"
+            "more space than the left side.\n"
+            "\n"
+            "| before <color:blue>Blue Text</color> after |\n"
+            "| before           Blue Text           after |\n"
+            "\n"
+            "# center_line\n"
+            "\n"
+            "Centers the line within the bounds.\n"
+            "\n"
+            "| before <color:blue>Blue Text</color> after |\n"
+            "|           before Blue Text after           |\n"
+        )
+    )
+
     args: argparse.Namespace = parser.parse_args()
     args_dict: dict[str, Any] = vars(args)
+
+    # The following error checks should never happen due to the parser
+    # alredy checking for the errors.
+    # Still done here to not assume errors.
+    mode_str: str | None = args_dict.get("mode")
+    if mode_str is None:
+        raise ValueError("No 'mode' setting passed by the argument parser.")
+    mode: ReplaceMode | None = ReplaceMode.get_by_value(mode_str)
+    if mode is None: 
+        raise ValueError("Invalid mode passed")
+    args_dict["mode"] = mode
+
     cli_args: CliArgs = CliArgs(**args_dict)
 
     return cli_args
--- src/utxterm/_replace_formatting.py
+++ src/utxterm/_replace_formatting.py
@@ -1,7 +1,86 @@
+from __future__ import annotations
 import re
-from typing import Callable
+from typing import Callable, assert_never
+from functools import partial
+from enum import StrEnum, auto
+from dataclasses import dataclass
 
 from utxterm._colors import ColorDict, AnsiFormat, InvalidColor
+
+
+@dataclass
+class Tag:
+    before: str
+    content: str
+    after: str
+
+    tagname: str
+    tagvalue: str | None
+
+    @staticmethod
+    def get_regex_pattern(tagname: str, boundary_str: str = "\u2502") -> str:
+        return (
+            f"(?<={boundary_str})(?P<before>[^{boundary_str}]*?)"
+            f"<(?P<tagname>{tagname})(:(?P<tagvalue>[^>]*?))?>"
+            f"(?P<content>[^{boundary_str}]*?)"
+            f"</{tagname}>"
+            f"(?P<after>[^{boundary_str}]*?)(?={boundary_str})"
+        )
+
+    @staticmethod
+    def from_match(match: re.Match):
+        return Tag(**match.groupdict())
+
+
+class ReplaceMode(StrEnum):
+    #: Simply replace every formatting sequence with a space.
+    #:
+    #: # Example:
+    #:
+    #: | before <color:blue>Blue Text</color> after |
+    #: | before             Blue Text         after |
+    simple = auto()
+
+    #: The replacement whitespace with will be added to the right of the
+    #: **all following** text.
+    #:
+    #: # Example:
+    #:
+    #: | before <color:blue>Blue Text</color> after |
+    #: | before Blue Text after                     |
+    align_left = auto()
+
+    #: Centers the text **within** the replacement whitespace.
+    #:
+    #: In case of uneven whitespace, the right side will contain one more
+    #: space than the left side.
+    #:
+    #: # Example:
+    #:
+    #: | before <color:blue>Blue Text</color> after |
+    #: | before           Blue Text           after |
+    center_ws = auto()
+
+    #: Centers the line within the bounds.
+    #:
+    #: # Example:
+    #:
+    #: | before <color:blue>Blue Text</color> after |
+    #: |           before Blue Text after           |
+    center_line = auto()
+
+    @staticmethod
+    def as_list() -> list[str]:
+        return [mode.value for mode in ReplaceMode]
+
+    @staticmethod
+    def get_by_value(
+        value: str, default: ReplaceMode | None = None
+    ) -> ReplaceMode | None:
+        for mode in ReplaceMode:
+            if mode.value == value:
+                return mode
+        return default
 
 
 def get_ansi_color(color_spec: str) -> str:
@@ -20,95 +99,164 @@ def get_ansi_color(color_spec: str) -> str:
     )
 
 
-def _replace_single_arg_match(
+def _replace_single_tag(
     match: re.Match,
-    num_replacement_spaces: int,
-    text_fmt_func: Callable[[str,],str,],
+    text_fmt_func: Callable[[str, str|None],str,],
+    mode: ReplaceMode,
 ) -> str:  # fmt: skip
-    text: str = match.group(1)
+    tag: Tag = Tag.from_match(match)
 
-    all_groups: list[str | None] = list(match.groups())
-    all_groups = all_groups[1:]
-    all_groups: list[str] = [grp for grp in all_groups if grp is not None]
+    content: str = text_fmt_func(tag.content, tag.tagvalue)
 
-    other_content: str = "".join(all_groups)
+    match mode:
+        case ReplaceMode.simple:
+            num_left_spaces: int = len("<>") + len(tag.tagname)
+            if tag.tagvalue is not None:
+                num_left_spaces += len(":") + len(tag.tagvalue)
+            left_spaces: str = " " * num_left_spaces
+            right_spaces: str = " " * (len("</>") + len(tag.tagname))
 
-    replacement_spaces: str = " " * num_replacement_spaces
-    fmt_text: str = text_fmt_func(text)
+            return (
+                f"{tag.before}{left_spaces}{content}{right_spaces}{tag.after}"
+            )
 
-    return f"{fmt_text}{other_content}{replacement_spaces}"
+        case ReplaceMode.align_left:
+            num_spaces: int = len("<></>") + len(tag.tagname) * 2
+            if tag.tagvalue is not None:
+                num_spaces += len(":") + len(tag.tagvalue)
+            spaces: str = " " * num_spaces
+            return f"{tag.before}{content}{tag.after}{spaces}"
+
+        case ReplaceMode.center_ws:
+            num_spaces: int = len("<></>") + len(tag.tagname) * 2
+            if tag.tagvalue is not None:
+                num_spaces += len(":") + len(tag.tagvalue)
+            num_spaces_left: int = num_spaces // 2
+            num_spaces_right: int = num_spaces // 2
+
+            if num_spaces % 2 == 1:
+                num_spaces_right += 1
+
+            left_spaces: str = " " * num_spaces_left
+            right_spaces: str = " " * num_spaces_right
+
+            return (
+                f"{tag.before}{left_spaces}{content}{right_spaces}{tag.after}"
+            )
+
+        case ReplaceMode.center_line:
+            num_spaces: int = len("<></>") + len(tag.tagname) * 2
+            if tag.tagvalue is not None:
+                num_spaces += len(":") + len(tag.tagvalue)
+
+            trailing_spaces: re.Match | None = re.search(" *$", tag.after)
+            leading_spaces: re.Match | None = re.search("^ *", tag.before)
+
+            if trailing_spaces is not None:
+                num_spaces += len(trailing_spaces.group(0))
+                tag.after = tag.after.rstrip()
+
+            if leading_spaces is not None:
+                num_spaces += len(leading_spaces.group(0))
+                tag.before = tag.before.lstrip()
+
+            num_spaces_left: int = num_spaces // 2
+            num_spaces_right: int = num_spaces // 2
+
+            if num_spaces % 2 == 1:
+                num_spaces_right += 1
+
+            left_spaces: str = " " * num_spaces_left
+            right_spaces: str = " " * num_spaces_right
+
+            return (
+                f"{left_spaces}{tag.before}{content}{tag.after}{right_spaces}"
+            )
+
+        case _:
+            assert_never(mode)
 
 
-def replace_bold(match: re.Match) -> str:
-    return _replace_single_arg_match(
-        match=match,
-        num_replacement_spaces=7,
-        text_fmt_func=AnsiFormat.bold,
+def _re_sub_single_tag(
+    content: str,
+    tag: str,
+    text_fmt_func: Callable[[str, str|None],str,],
+    mode: ReplaceMode,
+) -> str:  # fmt: skip
+    replace_fnc = partial(
+        _replace_single_tag, text_fmt_func=text_fmt_func, mode=mode
     )
 
-
-def replace_italic(match: re.Match) -> str:
-    return _replace_single_arg_match(
-        match=match,
-        num_replacement_spaces=7,
-        text_fmt_func=AnsiFormat.italic,
-    )
-
-
-def replace_underline(match: re.Match) -> str:
-    return _replace_single_arg_match(
-        match=match,
-        num_replacement_spaces=7,
-        text_fmt_func=AnsiFormat.underline,
-    )
-
-
-def replace_strikethrough(match: re.Match) -> str:
-    return _replace_single_arg_match(
-        match=match,
-        num_replacement_spaces=7,
-        text_fmt_func=AnsiFormat.strikethrough,
-    )
-
-
-def replace_color(match: re.Match) -> str:
-    color_spec = match.group(1)
-    content = match.group(2)
-    ansi_code = get_ansi_color(color_spec)
-
-    all_groups: list[str | None] = list(match.groups())
-    all_groups = all_groups[2:]
-    all_groups: list[str] = [grp for grp in all_groups if grp is not None]
-
-    other_content: str = "".join(all_groups)
-
-    replacement_spaces: str = " " * (16 + len(color_spec))
-
-    return (
-        f"{ansi_code}{content}{AnsiFormat.reset_fg}"
-        f"{other_content}{replacement_spaces}"
-    )
-
-
-def replace_single_sweep(content: str) -> str:
-    content = re.sub(r"<b>(.*?)</b>(.*?)(?=\u2502)", replace_bold, content)
-    content = re.sub(r"<i>(.*?)</i>(.*?)(?=\u2502)", replace_italic, content)
-    content = re.sub(r"<u>(.*?)</u>(.*?)(?=\u2502)", replace_underline, content)
-    content = re.sub(
-        r"<s>(.*?)</s>(.*?)(?=\u2502)", replace_strikethrough, content
-    )
-    content = re.sub(
-        r"<color:(.*?)>(.*?)</color>(.*?)(?=\u2502)",
-        replace_color,
-        content,
-    )
+    pattern: str = Tag.get_regex_pattern(tagname=tag)
+    content = re.sub(pattern, replace_fnc, content)
     return content
 
 
-def replace_loop(content: str) -> str:
+def replace_bold(content: str, mode: ReplaceMode) -> str:
+    return _re_sub_single_tag(
+        content=content,
+        tag="b",
+        text_fmt_func=AnsiFormat.bold,
+        mode=mode,
+    )
+
+
+def replace_italic(content: str, mode: ReplaceMode) -> str:
+    return _re_sub_single_tag(
+        content=content,
+        tag="i",
+        text_fmt_func=AnsiFormat.italic,
+        mode=mode,
+    )
+
+
+def replace_underline(content: str, mode: ReplaceMode) -> str:
+    return _re_sub_single_tag(
+        content=content,
+        tag="u",
+        text_fmt_func=AnsiFormat.underline,
+        mode=mode,
+    )
+
+
+def replace_strikethrough(content: str, mode: ReplaceMode) -> str:
+    return _re_sub_single_tag(
+        content=content,
+        tag="s",
+        text_fmt_func=AnsiFormat.strikethrough,
+        mode=mode,
+    )
+
+
+def _replace_color_fmt(content: str, color_spec: str | None):
+    if color_spec is None:
+        raise ValueError("Used color tag without a color value")
+    ansi_code = get_ansi_color(color_spec)
+    return f"{ansi_code}{content}{AnsiFormat.reset_fg}"
+
+
+def replace_color(content: str, mode: ReplaceMode) -> str:
+    return _re_sub_single_tag(
+        content=content,
+        tag="color",
+        text_fmt_func=_replace_color_fmt,
+        mode=mode,
+    )
+
+
+def replace_single_sweep(content: str, mode: ReplaceMode) -> str:
+    content = replace_bold(content, mode)
+    content = replace_italic(content, mode)
+    content = replace_underline(content, mode)
+    content = replace_strikethrough(content, mode)
+    content = replace_color(content, mode)
+    return content
+
+
+def replace_loop(content: str, mode: ReplaceMode) -> str:
     content_changed: bool = True
     while content_changed:
-        new_content: str = replace_single_sweep(content)
+        new_content: str = replace_single_sweep(content, mode)
         content_changed = content == new_content
         content = new_content
     return content
